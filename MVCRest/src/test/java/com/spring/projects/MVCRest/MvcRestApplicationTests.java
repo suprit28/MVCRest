de27a234b8787552f2cae1c3cecb325c5e77fc8e
SUPRIT CHAUDHARI <supritchaudhari.iitg@gmail.com>
2022-09-05 13:36:57 +0530

    project commit

---
+++ MVCRest/src/test/java/com/spring/projects/MVCRest/MvcRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.projects.MVCRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
